c7a948a98f48db9e885845ec5b5ed159a4e085ba
ryannnkl@gmail.com <ryannnkl@gmail.com>
2024-01-09 09:19:20 -0300

    change: initial step to documentation

--- src/main/java/com/example/crud/controller/ProductController.java
+++ src/main/java/com/example/crud/controller/ProductController.java
@@ -3,6 +3,11 @@ package com.example.crud.controller;
 import com.example.crud.model.Product;
 import com.example.crud.service.ProductService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,19 +23,23 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @Operation(summary = "Lista os produtos existentes", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
+            @ApiResponse(responseCode = "505", description = "Erro interno", content = @Content(mediaType = "null", schema = @Schema(hidden = true))),
+    })
     @GetMapping
-    public ResponseEntity<List<Product>> findAll(){
+    public ResponseEntity<List<Product>> findAll() {
         try {
             var products = productService.findAll();
             return ResponseEntity.status(HttpStatus.OK).body(products);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<Product>> findById(@PathVariable UUID id){
+    public ResponseEntity<Optional<Product>> findById(@PathVariable UUID id) {
         var product = productService.findById(id);
         if (product.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
@@ -40,18 +49,18 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> create(@RequestBody Product product){
+    public ResponseEntity<Product> create(@RequestBody Product product) {
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(product));
     }
 
     @PutMapping
-    public ResponseEntity<Product> update(@RequestBody Product product){
+    public ResponseEntity<Product> update(@RequestBody Product product) {
         return ResponseEntity.status(HttpStatus.OK).body(productService.update(product));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete(@PathVariable UUID id){
+    public ResponseEntity<?> delete(@PathVariable UUID id) {
         productService.deleteById(id);
-        return ResponseEntity.status(HttpStatus.OK).body("Deletado com sucesso");
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
