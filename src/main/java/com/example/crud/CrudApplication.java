1ee5200c67b19c7af14e526be1427d0d712944e4
ryannnkl@gmail.com <ryannnkl@gmail.com>
2024-01-09 09:13:25 -0300

    change: update to swagger documentation

--- src/main/java/com/example/crud/CrudApplication.java
+++ src/main/java/com/example/crud/CrudApplication.java
@@ -1,9 +1,14 @@
 package com.example.crud;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@OpenAPIDefinition(info = @Info(title = "Swagger api", version = "1", description = "Exemplo de api com documentação"))
 @SpringBootApplication
+@EnableScheduling
 public class CrudApplication {
 
     public static void main(String[] args) {
