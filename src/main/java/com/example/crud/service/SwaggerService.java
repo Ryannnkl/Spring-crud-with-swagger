b847cd13a080a1240f634116963f5ef27f133eb3
ryannnkl@gmail.com <ryannnkl@gmail.com>
2024-01-09 09:13:40 -0300

    add: Swagger service

--- src/main/java/com/example/crud/service/SwaggerService.java
+++ src/main/java/com/example/crud/service/SwaggerService.java
@@ -1,2 +1,20 @@
-package com.example.crud.service;public class SwaggerService {
+package com.example.crud.service;
+
+import com.example.crud.model.Product;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+@Component
+public class SwaggerService {
+    public  List<Product> findProducts() {
+        return Arrays.asList(
+                new Product("Harry potter e a pedra filosofal", "um menino que descobre que é um bruxo",45.0, 3),
+                new Product("Harry potter e a camara secreta", "inimigos do herdeiro cuidado, a camara secreta foi aberta",45.0, 3),
+                new Product("Harry potter e o prizioneiro de askaban", "Um rato que tem 1 dedo a menos e vive a mais de 12 anos não levanta suspeita na familia Weasley",45.0, 3)
+                );
+    }
 }
